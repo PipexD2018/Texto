e68f03cb62426996e94c9aa44c221095a0f1697c
Felipe Araque <l-felipe-araque@hotmail.com>
2019-10-27 17:46:57 -0500

    Texto
    Lee y escribe archivos de texto

--- Archivotexto/src/archivotexto/principal.java
+++ Archivotexto/src/archivotexto/principal.java
@@ -6,6 +6,11 @@
  */
 package archivotexto;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author Pipe Araque
@@ -13,14 +18,22 @@ package archivotexto;
 public class principal {
  
     
-     public static void main(String[] args) {
+     public static void main(String[] args) throws ClassNotFoundException {
         
-         
-         Componentes co = new Componentes();
-         co.setVisible(true);
-         
-         //metodo me = new metodo();
-         //me.Escribir("Primerarchivo.txt");
-         //me.Leer("Primerarchivo.txt");
+         try {
+             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+             Componentes co = new Componentes();
+             co.setVisible(true);
+             
+             //metodo me = new metodo();
+             //me.Escribir("Primerarchivo.txt");
+             //me.Leer("Primerarchivo.txt");
+         } catch (InstantiationException ex) {
+             Logger.getLogger(principal.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (IllegalAccessException ex) {
+             Logger.getLogger(principal.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (UnsupportedLookAndFeelException ex) {
+             Logger.getLogger(principal.class.getName()).log(Level.SEVERE, null, ex);
+         }
     }
 }
